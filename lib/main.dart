ba4b933590273f12e2dffd5ce98a59652634323f
Timmy Chuang <yuray0426@gmail.com>
2023-05-18 18:09:05 +0800

    add -> screenutils

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'package:project_fitness/common/index.dart';
@@ -15,24 +16,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      // 主題
-      theme: ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
+    return ScreenUtilInit(
+        designSize: const Size(414, 896), // 設計稿中設備的尺寸(單位隨意,建議dp,但在使用過程中必須保持一致)
+        splitScreenMode: false, // 支持分屏尺寸
+        minTextAdapt: false, // 是否根據寬度/高度中的最小值適配文字
+        // 一般返回一個material app類型的function()
+        builder: (context, child) {
+          return GetMaterialApp(
+            title: 'Flutter Demo',
+            // 主題
+            theme:
+                ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
 
-      // 系統語言
-      translations: Translation(), // 辭典
-      localizationsDelegates: Translation.localizationsDelegates, // 代理
-      supportedLocales: Translation.supportedLocales, // 支援的語系
-      locale: ConfigService.to.locale, // 當前語言種類
-      fallbackLocale: Translation.fallbackLocale, // 當前語言種類
+            // 系統語言
+            translations: Translation(), // 辭典
+            localizationsDelegates: Translation.localizationsDelegates, // 代理
+            supportedLocales: Translation.supportedLocales, // 支援的語系
+            locale: ConfigService.to.locale, // 當前語言種類
+            fallbackLocale: Translation.fallbackLocale, // 當前語言種類
 
-      // 路由
-      initialRoute: RouteNames.stylesStylesIndex,
-      getPages: RoutePages.list,
-      navigatorObservers: [RoutePages.observer],
+            // 路由
+            initialRoute: RouteNames.systemSplash,
+            // initialRoute: RouteNames.stylesStylesIndex,
+            getPages: RoutePages.list,
+            navigatorObservers: [RoutePages.observer],
 
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+            // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          );
+        });
   }
 }
