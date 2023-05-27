1d2520d63242047f6bb654447acae82cea44ba8f
Timmy Chuang <10946021@ntub.edu.tw>
2023-05-27 23:48:00 +0800

    add -> http request

--- lib/common/services/wp_http.dart
+++ lib/common/services/wp_http.dart
@@ -9,7 +9,7 @@ class WPHttpService extends GetxService {
   static WPHttpService get to => Get.find();
 
   late final Dio _dio;
-  // final CancelToken _cancelToken = CancelToken(); // 默认去掉
+  // final CancelToken _cancelToken = CancelToken(); // 默認去掉
 
   @override
   void onInit() {
@@ -26,7 +26,7 @@ class WPHttpService extends GetxService {
     );
     _dio = Dio(options);
 
-    // 拦截器
+    // 攔截器
     _dio.interceptors.add(RequestInterceptors());
   }
 
@@ -95,28 +95,28 @@ class WPHttpService extends GetxService {
   }
 }
 
-/// 拦截
+/// 攔截
 class RequestInterceptors extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     // super.onRequest(options, handler);
 
-    // http header 头加入 Authorization
+    // http header 頭加入 Authorization
     // if (UserService.to.hasToken) {
     //   options.headers['Authorization'] = 'Bearer ${UserService.to.token}';
     // }
 
     return handler.next(options);
-    // 如果你想完成请求并返回一些自定义数据，你可以resolve一个Response对象 `handler.resolve(response)`。
-    // 这样请求将会被终止，上层then会被调用，then中返回的数据将是你的自定义response.
+    //如果你想完成請求並返回一些自定義數據，你可以resolve一個Response對象 `handler.resolve(response)`。
+    //這樣請求將會被終止，上層then會被調用，then中返回的數據將是你的自定義response.
     //
-    // 如果你想终止请求并触发一个错误,你可以返回一个`DioError`对象,如`handler.reject(error)`，
-    // 这样请求将被中止并触发异常，上层catchError会被调用。
+    //如果你想終止請求並觸發一個錯誤,你可以返回一個`DioError`對象,如`handler.reject(error)`，
+    //這樣請求將被中止並觸發異常，上層catchError會被調用。
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    // 200 请求成功, 201 添加成功
+    // 200 請求成功, 201 添加成功
     if (response.statusCode != 200 && response.statusCode != 201) {
       handler.reject(
         DioError(
@@ -131,7 +131,7 @@ class RequestInterceptors extends Interceptor {
     }
   }
 
-  // 退出并重新登录
+  // 退出並重新登錄
   // Future<void> _errorNoAuthLogout() async {
   //   await UserService.to.logout();
   //   Get.toNamed(RouteNames.systemLogin);
@@ -141,14 +141,14 @@ class RequestInterceptors extends Interceptor {
   Future<void> onError(DioError err, ErrorInterceptorHandler handler) async {
     final exception = HttpException(err.message ?? "error message");
     switch (err.type) {
-      case DioErrorType.badResponse: // 服务端自定义错误体处理
+      case DioErrorType.badResponse: // 服務端自定義錯誤體處理
         {
           // final response = err.response;
           // final errorMessage = ErrorMessageModel.fromJson(response?.data);
           //   switch (errorMessage.statusCode) {
-          //     // 401 未登录
+          //     // 401 未登錄
           //     case 401:
-          //       // 注销 并跳转到登录页面
+          //       // 註銷 並跳轉到登錄頁面
           //       _errorNoAuthLogout();
           //       break;
           //     case 404:
